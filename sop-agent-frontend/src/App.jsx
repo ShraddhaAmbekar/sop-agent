03d3f6f549a1d6350ffc61f08e178490949eefe7
ShraddhaAmbekar <147248222+ShraddhaAmbekar@users.noreply.github.com>
2026-01-16 13:04:37 +0530

    frontend basic folder structure and upload pdf button is ready

---
+++ sop-agent-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import AdminPage from "./pages/AdminPage";
+import './App.css'
+
+function App() {
+
+  return (
+  <>
+  <h1>SOP Agent App</h1>
+<AdminPage />
+  </>
+  )
+}
+
+export default App
---
+++ sop-agent-frontend/src/pages/AdminPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import FileUploader from "../components/FileUploader";
+
+export default function AdminPage() {
+  return (
+    <div>
+      <h2>Admin Panel</h2>
+      <FileUploader />
+    </div>
+  );
+}
